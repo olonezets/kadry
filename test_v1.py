e525a7fef7091844ef4e0fd3b7e9b909764794b9
Anatolii Babkin <olonezets@mail.ru>
2022-03-18 14:14:58 +0200

    test

---
+++ test_v1.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+def numOfDays(date1, date2):
+
+    return (date2-date1).days
+
+
+date1 = datetime.datetime(
+    int(input('enter_a_first_year:')),
+    int(input('enter_a_first_month:')),
+    int(input('enter_a_first_day:'))
+    )
+
+date2 = datetime.datetime(
+    int(input('enter_a_last_year:')),
+    int(input('enter_a_last_month:')),
+    int(input('enter_a_last_day:'))2017
+    )
+
+print(numOfDays(date1, date2), "days_betwen_date")
+
+x1 = numOfDays(date1, date2)
+x2 = x1 * 0.082
+
+print((x2), "days_otpusk_for_date")
